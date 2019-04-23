fb98477d1046f410064613506f6fbefd1251dbe6
Michael <metamike14@gmail.com>
2019-04-23 14:14:49 -0700

    Worked on functions, need to add the flag option.

--- main.cpp
+++ main.cpp
@@ -3,32 +3,54 @@
 
 using namespace std;
 
+bool legv8Flag[false, false, false, false];
+
 void ADD(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
-    
+    if(typeR){
+        destination = source1 + source2;
+    }else{
+        destination = source1 + immediate;
+    }
 }
 
 void SUB(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
-
+    if(typeR){
+        destination = source1 - source2;
+    }else{
+        destination = source1 - immediate;
+    }
 }
 
 void AND(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
-
+    if(typeR){
+        destination = source1 & source2;
+    }else{
+        destination = source1 & immediate;
+    }
 }
 
 void ORR(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
-
+    if(typeR){
+        destination = source1 | source2;
+    }else{
+        destination = source1 | immediate;
+    }
 }
 
 void EOR(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
-
+    if(typeR){
+        destination = source1 ^ source2;
+    }else{
+        destination = source1 ^ immediate;
+    }
 }
 
 void LSL(long long int &source1, long long int shamt, long long int &destination){
-
+    destination = source1 << shamt;
 }
 
 void LSR(long long int &source1, long long int shamt, long long int &destination){
-
+    destination = source1 >> shamt;
 }
 
 void STUR(int size, long long int &source1, long long int addr, long long int &destination){
@@ -43,6 +65,10 @@ void LDUR(int size, long long int &source1, long long int addr, long long int &d
 int main() {
     vector<long long int> RFILE;
     vector<int8_t> MEM;
+    RFILE.push_back(20);
+    RFILE.push_back(50);
+    RFILE.push_back(0);
+    ADD(true, RFILE[0], RFILE[0], 0, RFILE[2], false);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
