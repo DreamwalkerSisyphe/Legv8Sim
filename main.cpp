5537885ac7eccf97ec677d316151cae45139633e
Michael <metamike14@gmail.com>
2019-04-23 13:46:46 -0700

    Base

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void ADD(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
+    
+}
+
+void SUB(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
+
+}
+
+void AND(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
+
+}
+
+void ORR(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
+
+}
+
+void EOR(bool typeR, long long int &source1, long long int &source2, long long int immediate, long long int &destination, bool flag){
+
+}
+
+void LSL(long long int &source1, long long int shamt, long long int &destination){
+
+}
+
+void LSR(long long int &source1, long long int shamt, long long int &destination){
+
+}
+
+void STUR(int size, long long int &source1, long long int addr, long long int &destination){
+
+}
+
+void LDUR(int size, long long int &source1, long long int addr, long long int &destination){
+
+}
+
+
+int main() {
+    vector<long long int> RFILE;
+    vector<int8_t> MEM;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
